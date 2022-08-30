61826ee059d074f1f00073adcdedefab565ed82b
Olamilekan <olamilekanomosanya@gmail.com>
2022-08-30 17:32:21 +0100

    add start finction

---
+++ js/app.js
@@ -0,0 +1,14 @@
+let score  = parseInt(0);
+let formNum = parseInt(0);
+let ansBlock;
+let correctAns = [1,1,1];
+let ans = [];
+let ques;
+
+
+function start(){
+    document.getElementById("msg").style.display ="none"
+    document.getElementById("startBtn").style.display ="none"
+    document.getElementById("headImg").src = "img/dice2.gif"
+
+}
